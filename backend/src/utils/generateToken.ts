affe8a5632f9f494bc25cbedfb03f5cdfb6006d0
Harman6282 <harmans6282@gmail.com>
2025-05-31 19:35:56 +0530

    format

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -3,22 +3,25 @@ import { JWT_SECRET } from "../secrets";
 import { Response } from "express";
 
 export const generateToken: any = (user: any, res: Response) => {
-  const token = jwt.sign({ 
-    id: user.id,
-    email: user.email,
-   }, JWT_SECRET!, {
-    expiresIn: "15d",
-  });
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    JWT_SECRET!,
+    {
+      expiresIn: "15d",
+    }
+  );
 
-   res.cookie("accessToken", token, {
+  res.cookie("accessToken", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
   });
 };
 
-
 export const decodeJWT = (token: string) => {
   let decoded = jwt.decode(token);
   return decoded;
-}
+};
