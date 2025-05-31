d06519df9bb7d93da71c34aaa875bdcde3d10694
Harman6282 <harmans6282@gmail.com>
2025-05-31 15:39:55 +0530

    prisma init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,16 @@ dotenv.config();
 import express from "express";
 import rootRouter from "./routes";
 import { PORT } from "./secrets";
+import { PrismaClient } from "@prisma/client";
+import { log } from "console";
 
 const app = express();
 
 app.use(express.json());
+
+export const prisma = PrismaClient({
+  log: ["query"]
+})
 
 app.use("/api", rootRouter);
 
