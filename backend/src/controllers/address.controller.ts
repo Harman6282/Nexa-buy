0f34f8c7742f86ec51ee520923b623778aaa8290
Harman singh <harmans6282@gmail.com>
2025-06-12 21:56:14 +0530

    add address controller

--- backend/src/controllers/address.controller.ts
+++ backend/src/controllers/address.controller.ts
@@ -1,11 +1,48 @@
 import { Request, Response } from "express";
+import { CreateAddressSchema } from "../schema/users";
+import { prisma } from "..";
+import { JwtPayload } from "jsonwebtoken";
+import { ApiError } from "../utils/apiError";
 
-export const createAddress: any = async (req: Request, res: Response) => {};
+export const createAddress: any = async (req: Request, res: Response) => {
+  const userId = (req.user as JwtPayload).id;
+
+  const parsed = CreateAddressSchema.safeParse(req.body);
+  if (!parsed.success) {
+     throw new ApiError(
+      400,
+      parsed.error.errors[0].message,
+      parsed.error.errors.map((error) => error.path[0] + ": " + error.message)
+    );
+  }
+  const { lineOne, lineTwo, city, pincode, country, state } = parsed.data;
+
+  const address = await prisma.address.create({
+    data: {
+      lineOne,
+      lineTwo,
+      city,
+      state,
+      pincode,
+      country,
+      userId,
+    },
+  });
+
+  if (!address) {
+    return res.status(500).json({
+      error: "Failed to create address",
+    });
+  }
+
+  return res.status(201).json({
+    message: "Address created successfully",
+    address,
+  });
+};
 
 export const updateAddress: any = async (req: Request, res: Response) => {};
 
-
 export const deleteAddress: any = async (req: Request, res: Response) => {};
-
 
 export const getAllAddresses: any = async (req: Request, res: Response) => {};
--- backend/src/schema/users.ts
+++ backend/src/schema/users.ts
@@ -1,3 +1,4 @@
+import { stat } from "fs";
 import { z } from "zod";
 
 export const SignUpSchema = z.object({
@@ -9,4 +10,13 @@ export const SignUpSchema = z.object({
 export const LoginSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export const CreateAddressSchema = z.object({
+  lineOne: z.string().min(1, "Address field one is required"),
+  lineTwo: z.string().optional(),
+  city: z.string().min(1, "City is required"),
+  state: z.string().min(1, "State is required"),
+  pincode: z.string().min(1, "Pin code is required"),
+  country: z.string().min(1, "Country is required"),
 });
