eec991b9ab12ab08b6f08b97a3eeba99ef5224a7
Harman singh <harmans6282@gmail.com>
2025-06-14 23:50:32 +0530

    create category route

--- backend/src/routes/admin.route.ts
+++ backend/src/routes/admin.route.ts
@@ -1,11 +1,18 @@
-
-
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { adminCheck, authenticate } from "../middlewares/auth/authenticate";
-import { updateOrderStatus } from "../controllers/admin/admin.controller";
+import {
+  createCategory,
+  updateOrderStatus,
+} from "../controllers/admin/admin.controller";
 
 const adminRoutes: Router = Router();
 
 adminRoutes.put("/orderstatus/:id", asyncHandler(updateOrderStatus));
+adminRoutes.post(
+  "/category",
+  authenticate,
+  adminCheck,
+  asyncHandler(createCategory)
+);
 export default adminRoutes;
